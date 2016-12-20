0fec68f3f1919ca19470020f2ac7eb623c5c9909
kevin duan <agonyice0115@gmail.com>
2016-12-20 17:20:41 +0800

    create project

---
+++ start.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify
+from flask import render_template
+from flask import request
+
+app = Flask(__name__)
+
+
+## login
+@app.route('/')
+@app.route('/<name>')
+def index(name=None):
+    error = None
+    return render_template("index.html", name=name)
+
+@app.route('/shoping')
+def shopping():
+    food=["chiness","chsss","kdjdjjd","kskskskksk"]
+    ##return render_template("shoping.html",food=food)
+    ## return json
+    return jsonify("food",food[0])
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
